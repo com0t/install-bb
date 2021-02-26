7a5a9962a78d36b80b0c9fa500771fb9dbf78aa8
Nguyen Khanh <nguyen.van.khanh-b@sun-asterisk.com>
2021-02-26 16:18:40 +0700

    rewrite install

--- install.sh
+++ install.sh
@@ -1,115 +1,134 @@
-# Install environment
-root_dir=`pwd`
-sudo apt update -y
+#!/bin/bash
+
 ## Check apt packet
 if [[ -z `which apt` ]]; then
 	echo "[!] apt packet not found" | tee  $root_dir/install-bb.log
-	exit 0
-fi
-## Install golang
-if [[ -z `which go` ]]; then
-	filego="go1.14.2.linux-amd64.tar.gz"
-	wget "https://dl.google.com/go/$filego"
-	if [[ ! -f $filego ]]; then
-		echo "Don't downloaded $filego" | tee -a  $root_dir/install-bb.log
-		exit 0
-	fi
-	sudo tar -C /usr/local -xzf $filego
-	if [[ -z "`cat $HOME/.profile | grep GOROOT`" ]]; then
-		sudo echo 'export GOROOT="/usr/local/go"' >> $HOME/.profile
-	fi
-	if [[ -z "`cat $HOME/.profile | grep GOPATH`" ]]; then
-		sudo echo 'export GOPATH="$HOME/go"' >> $HOME/.profile
-	fi
-	if [[ -z "`cat $HOME/.profile | grep GOROOT/bin`" ]]; then
-		sudo echo 'export PATH="$PATH:$GOPATH/bin:$GOROOT/bin"' >> $HOME/.profile
-	fi
-	source $HOME/.profile
-	rm -f $filego
-else
-	echo "[+] GoLang exists" | tee -a  $root_dir/install-bb.log
-fi
-## Install pip3 and pip
-if [[ -z `which pip3` ]]; then
-	sudo apt install -y python3-pip
-else
-	echo "[+] pip3 exists" | tee -a  $root_dir/install-bb.log
+	exit 1
 fi
 
-if [[ -z `which pip` ]]; then
-	sudo apt install -y python-pip
+# Install environment
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y jq
+sudo apt-get install -y ruby-full ruby
+sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y libldns-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-dnspython
+sudo apt-get install -y git
+sudo apt-get install -y rename
+sudo apt-get install -y xargs
+
+## Install Tools
+sudo apt-get install -y sqlmap nmap awscli
+
+
+## Install golang
+if [[ -z `which go` ]]; then
+	filego="go1.16.linux-amd64.tar.gz"
+	wget "https://dl.google.com/go/$filego"
+	if [[ ! -f $filego ]]; then
+		echo "Don't downloaded $filego"
+		exit 1
+	fi
+	sudo tar -C /usr/local -xzf $filego
+
+	if [[ -z "`cat $HOME/.profile | grep GOROOT`" ]]; then
+		sudo echo 'export GOROOT="/usr/local/go"' >> $HOME/.bashrc
+	fi
+	if [[ -z "`cat $HOME/.profile | grep GOPATH`" ]]; then
+		sudo echo 'export GOPATH="$HOME/go"' >> $HOME/.bashrc
+	fi
+	if [[ -z "`cat $HOME/.profile | grep GOROOT/bin`" ]]; then
+		sudo echo 'export PATH="$PATH:$GOPATH/bin:$GOROOT/bin"' >> $HOME/.bashrc
+	fi
+	source $HOME/.bashrc
+	rm -f $filego
 else
-	echo "[+] pip exists" | tee -a  $root_dir/install-bb.log
-fi
-# Install tool
-if [[ -z `which git` ]]; then
-	sudo apt install -y git
-else
-	echo "[+] Git exists" | tee -a  $root_dir/install-bb.log
+	echo "[+] GoLang exists"
 fi
 
 if [[ ! -d "$HOME/tools" ]]; then
 	mkdir $HOME/tools
-else
-	echo "[+] tools folder exists" | tee -a  $root_dir/install-bb.log
 fi
 
-cd $HOME/tools
-## Install dirsearch
-if [[ -z `which dirsearch` ]]; then
-	echo "[+] Install dirsearch" | tee -a  $root_dir/install-bb.log
-	git clone https://github.com/maurosoria/dirsearch.git
-	sed -i 's,/usr/bin/env python3,'"`which python3`," dirsearch/dirsearch.py
-	echo "Create sym link dirsearch" | tee -a  $root_dir/install-bb.log
-	sudo ln -s $HOME/tools/dirsearch/dirsearch.py /usr/local/bin/dirsearch
-else
-	echo "[+] dirsearch exists" | tee -a  $root_dir/install-bb.log
-fi
-## Install sqlmap	
-if [[ -z `which sqlmap` ]]; then
-	echo "[+] Install sqlmap" | tee -a  $root_dir/install-bb.log
-	sudo apt install -y sqlmap
-else
-	echo "[+] sqlmap exists" | tee -a  $root_dir/install-bb.log
-fi
-## Install nmap
-if [[ -z `which nmap` ]]; then
-	echo "[+] Install nmap" | tee -a  $root_dir/install-bb.log
-	sudo apt install -y nmap
-else
-	echo "[+] nmap exists" | tee -a  $root_dir/install-bb.log
-fi
-## Install amass
-if [[ -z `which amass` ]]; then
-	echo "[+] Install amass" | tee -a  $root_dir/install-bb.log
-	sudo snap install amass
-else
-	echo "[+] amass exists" | tee -a  $root_dir/install-bb.log
-fi
-## Install assetfinder
-if [[ -z `which assetfinder` ]]; then
-	echo "[+] Install assetfinder" | tee -a  $root_dir/install-bb.log
-	go get -u github.com/tomnomnom/assetfinder
-else
-	echo "[+] assetfinder exists" | tee -a  $root_dir/install-bb.log
-fi
-## Install httprobe
-if [[ -z `which httprobe` ]]; then
-	echo "[+] Install httprobe" | tee -a  $root_dir/install-bb.log
-	go get -u github.com/tomnomnom/httprobe
-else
-	echo "[+] httprobe exists" | tee -a  $root_dir/install-bb.log
-fi
-## Install paramspider
-if [[ -z `which paramspider` ]]; then
-	echo '[+] Install paramspider' | tee -a  $root_dir/install-bb.log
-	git clone https://github.com/devanshbatham/ParamSpider
-	cd ParamSpider
-	pip3 install -r requirements.txt
-	sed -i '1s,^,#!'"`which python3`"'\n,' paramspider.py
-	chmod +x paramspider.py
-	sudo ln -s $HOME/tools/ParamSpider/paramspider.py /usr/local/bin/paramspider
-	cd ..
-else
-	echo "[+] paramspider exists" | tee -a  $root_dir/install-bb.log
-fi
+echo 'Insstalling Nuclei'
+GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+echo 'Update template'
+nuclei -update-templates
+echo 'done'
+
+
+echo 'Installing Httpx'
+GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
+echo 'done'
+
+
+echo 'Installing Subfinder'
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+echo 'done'
+
+
+echo 'Installing Assetfinder'
+go get -u github.com/tomnomnom/assetfinder
+echo 'done'
+
+
+echo 'Installing Waybackurls'
+go get github.com/tomnomnom/waybackurls
+echo 'done'
+
+
+echo 'Installing Gf'
+go get -u github.com/tomnomnom/gf
+echo 'Install Gf-Patern'
+git clone https://github.com/1ndianl33t/Gf-Patterns.git
+mv Gf-Patterns ~/.gf
+echo 'done'
+
+
+echo 'Installing Subjack'
+go get github.com/haccer/subjack
+git clone https://github.com/haccer/subjack.git
+echo 'done'
+
+
+echo 'Installing Chromium'
+sudo snap install chromium
+echo 'done'
+
+
+echo 'Installing Aquatone'
+go get github.com/michenriksen/aquatone
+echo 'done'
+
+
+echo 'Installing Wpscan'
+sudo gem install wpscan
+echo 'done'
+
+
+echo 'Installing Dirsearch'
+git clone https://github.com/maurosoria/dirsearch.git
+echo 'done'
+
+
+echo 'Installing Massdns'
+git clone https://github.com/blechschmidt/massdns.git
+cd ~/tools/massdns
+make
+echo 'done'
+
+
+echo 'Downloading Seclists '
+git clone https://github.com/danielmiessler/SecLists.git
+cd ~/tools/SecLists/Discovery/DNS/
+## THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
+cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
+echo 'done'
